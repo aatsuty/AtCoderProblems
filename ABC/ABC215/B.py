8604b03a3c562b74a85c9fd5aed30c53baa4aa6a
freshmanfreshman <slyfoxdumbbunnyz@gmail.com>
2021-08-28 22:35:00 +0900

    ABC215_B.py

--- ABC/ABC215/B.py
+++ ABC/ABC215/B.py
@@ -1,5 +1,28 @@
 import math
+
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (low + high) //2
+        guess = list[mid]
+        if 2**guess <= item and 2**(guess+1) > item:
+            return mid
+        if 2**guess > item:
+            high = mid -1
+        else:
+            low = mid + 1
+    return None
+
+
+
+
 N = int(input())
-# shisu = int(math.log2(N))
-shisu = int(math.log(N,2))
-print(shisu)
+my_list = range(0,int(math.log2(10**20)))
+answer = binary_search(my_list, N)
+
+
+
+
+
+print(answer)
